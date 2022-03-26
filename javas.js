958f98ce1de94e96404996d2677376c475695dc0
brycen <brycenbb1996@gmail.com>
2022-03-26 13:55:11 +0000

    new card submissions work

--- javas.js
+++ javas.js
@@ -53,16 +53,7 @@ for(let i = 0; i < myLibrary.length;i++) {
     container.appendChild(bookcard);
 }
 
-console.log(document.getElementById('submit'));
-// document.getElementById('submit').addEventListener('submit', function(e) {
-//     console.log("Event listener for submit button");
-
-//     e.preventDefault();
-//     formSubmitted();
-// });
-
 document.getElementById('form').addEventListener('submit', function(event){
-    console.log("Event listener for submit button");
     event.preventDefault();
     event.stopPropagation();
     formSubmitted();
@@ -71,14 +62,44 @@ document.getElementById('form').addEventListener('submit', function(event){
 
 
 function formSubmitted() {
-    console.log("I made it here wooooooo");
-    console.log("222I made it here wooooooo");
     const form = document.querySelector("form");
     form.classList.add('hide');
+    //Processing form information
+    let inputs = document.getElementById("form").elements;
+    console.log(inputs);
+    // Author
+    if(inputs[1].value === ""){
+        inputs[1].value = "Unknown1"
+    }    
+    // Page count
+    if(inputs[2].value === ""){
+        console.log("page statement set to unknown")
+        inputs[2].value =99999;
+        console.log(inputs[2].value);
+
+        //input is number only, so it can only accept numbers as a value, not strings.
+    } 
+    //Genre
+    if(inputs[3].value === ""){
+        inputs[3].value = "Unknown3"
+    }
+    let bookcard = document.createElement("div");
+    bookcard.classList.add("card");
+    bookcard.textContent = new Book(inputs[0].value,inputs[1].value,inputs[2].value,inputs[4].value).info();
+    container.appendChild(bookcard);
+    // console.log(inputs[0].value);
+    // console.log(inputs[1].value);
+    // console.log(inputs[2].value);
+    // console.log(inputs[3].value);
+
+
+
+
     form.reset();
 
 }
 
+//New form created when the new book button is pressed, handled w/ innerhtml
 function newForm() {
     const form = document.querySelector("form");
     form.classList.remove('hide');
@@ -91,11 +112,13 @@ statusbutton.addEventListener('click', function() {
         statusbutton.textContent = "Not Read"
         statusbutton.classList.remove('read');
         statusbutton.classList.add('notread');
+        statusbutton.value = "false";
     }
     else{
         readStatus = true;
         statusbutton.textContent = "Read"
         statusbutton.classList.remove('notread');
         statusbutton.classList.add('read');
+        statusbutton.value = "true";
     }
 })
